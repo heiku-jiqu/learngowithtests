9d8d928003557a6b82e24e03170d107c9bb4e018
Heiser <heiser9295@hotmail.com>
2023-08-18 23:01:20 +0800

    refactor to another function

--- hello.go
+++ hello.go
@@ -12,14 +12,19 @@ func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	prefix := englishHelloPrefix
+	return greetingPrefix(language) + name + "!"
+}
+
+func greetingPrefix(language string) (prefix string) {
 	switch language {
 	case "Spanish":
 		prefix = spanishHelloPrefix
 	case "French":
 		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return prefix + name + "!"
+	return
 }
 
 func main() {
