59ab41eaceb2ae4a9a070c1c4816d2dba4018638
Heiser <heiser9295@hotmail.com>
2023-08-21 14:58:54 +0800

    add triangle and refactor tests

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -5,9 +5,19 @@ import "math"
 type Circle struct {
 	Radius float64
 }
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }
 
 func (r Rectangle) Area() float64 {
--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -21,17 +21,19 @@ func TestArea(t *testing.T) {
 		t.Helper()
 		got := shape.Area()
 		if got != want {
-			t.Errorf("got %g, want %g", got, want)
+			t.Errorf("got %g, want %g, with input %T%v", got, want, shape, shape)
 		}
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rect := Rectangle{12.0, 6.0}
-		want := 72.0
-		checkArea(t, rect, want)
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		want := 314.1592653589793
-		checkArea(t, circle, want)
-	})
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10}, 314.1592653589793},
+		{Triangle{12, 6}, 36.0},
+	}
+
+	for _, testCase := range areaTests {
+		checkArea(t, testCase.shape, testCase.want)
+	}
 }
