9058c5c8beeaca2c0cc3b7f758a5d3fc99dcbae3
Heiser <heiser9295@hotmail.com>
2023-08-21 16:18:16 +0800

    add withdraw

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -2,20 +2,31 @@ package wallet
 
 import "fmt"
 
-type Wallet struct {
-	balance int
-}
+type (
+	Bitcoin int
+	Wallet  struct {
+		balance Bitcoin
+	}
+)
 
-func (w *Wallet) Deposit(amt int) {
+func (w *Wallet) Deposit(amt Bitcoin) {
 	w.balance += amt
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
+func (w *Wallet) Withdraw(amt Bitcoin) {
+	w.balance -= amt
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 // arguments of functions are COPIED (passed by value)
-func (w Wallet) BrokenDeposit(amt int) {
+func (w Wallet) BrokenDeposit(amt Bitcoin) {
 	fmt.Printf("address of w.balance in Deposit() is %v \n", &w.balance)
 	w.balance += amt
 }
--- wallet/wallet_test.go
+++ wallet/wallet_test.go
@@ -6,16 +6,28 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	wallet.BrokenDeposit(10) // doesn't do anything since passed by val!
-	fmt.Printf("address of wallet.balance in test is %v \n", &wallet.balance)
+		wallet.BrokenDeposit(Bitcoin(10)) // doesn't do anything since passed by val!
+		fmt.Printf("address of wallet.balance in test is %v \n", &wallet.balance)
 
-	wallet.Deposit(10)
+		wallet.Deposit(10)
 
-	got := wallet.Balance()
-	want := 10
-	if got != want {
-		t.Errorf("got %d, want %d", got, want)
-	}
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(10)
+		got := wallet.Balance()
+		want := Bitcoin(10)
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
