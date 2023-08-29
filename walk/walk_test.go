8cce455d6e0b12d7b7c99c56212f98af2ef3693d
Heiser <heiser9295@hotmail.com>
2023-08-29 16:30:07 +0800

    handle array and slices

--- walk/walk_test.go
+++ walk/walk_test.go
@@ -57,6 +57,22 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"Chris", "London"},
 		},
+		{
+			"slices",
+			[]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
+		{
+			"arrays",
+			[2]Profile{
+				{33, "London"},
+				{34, "Reykjavík"},
+			},
+			[]string{"London", "Reykjavík"},
+		},
 	}
 
 	for _, test := range cases {
