c599b16ec6f5cdf5325ed2ce5502d46e81368f97
Heiser <heiser9295@hotmail.com>
2023-09-06 11:07:28 +0800

    parse title and description

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,12 +1,15 @@
 package blogposts
 
 import (
+	"bufio"
 	"io"
 	"io/fs"
+	"strings"
 )
 
 type Post struct {
-	Title string
+	Title       string
+	Description string
 }
 
 func NewPostsFromFS(filesys fs.FS) ([]Post, error) {
@@ -17,23 +20,41 @@ func NewPostsFromFS(filesys fs.FS) ([]Post, error) {
 
 	var posts []Post
 	for _, entry := range dirEntries {
-		post, _ := getPost(filesys, entry)
+		post, err := getPost(filesys, entry.Name())
+		if err != nil {
+			return nil, err
+		}
 		posts = append(posts, post)
 	}
 	return posts, nil
 }
 
-func getPost(filesys fs.FS, entry fs.DirEntry) (Post, error) {
-	file, err := filesys.Open(entry.Name())
+func getPost(filesys fs.FS, filename string) (Post, error) {
+	file, err := filesys.Open(filename)
 	if err != nil {
 		return Post{}, err
 	}
 	defer file.Close()
 
-	data, err := io.ReadAll(file)
-	if err != nil {
-		return Post{}, err
+	return newPost(file)
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+)
+
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	readMetaLine := func(prefix string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), prefix)
 	}
-	post := Post{Title: string(data[7:])}
+
+	post := Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+	}
 	return post, nil
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"hello/blogposts"
 	"io/fs"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
@@ -24,9 +25,15 @@ func TestNewBlogPosts(t *testing.T) {
 		}
 	})
 	t.Run("reads blogposts from FS", func(t *testing.T) {
+		const (
+			firstBody = `Title: Post 1
+Description: Description 1`
+			secondBody = `Title: Post 2
+Description: Description 2`
+		)
 		fs := fstest.MapFS{
-			"hello world.md":  {Data: []byte("Title: Post 1")},
-			"hello-world2.md": {Data: []byte("Title: Post 2")},
+			"hello world.md":  {Data: []byte(firstBody)},
+			"hello-world2.md": {Data: []byte(secondBody)},
 		}
 
 		posts, err := blogposts.NewPostsFromFS(fs)
@@ -37,9 +44,14 @@ func TestNewBlogPosts(t *testing.T) {
 			t.Errorf("got %d posts, want %d posts", len(posts), len(fs))
 		}
 		got := posts[0]
-		want := blogposts.Post{Title: "Post 1"}
-		if got != want {
-			t.Errorf("got %+v, want %+v", got, want)
-		}
+		want := blogposts.Post{Title: "Post 1", Description: "Description 1"}
+		assertPost(t, got, want)
 	})
+}
+
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
+	}
 }
